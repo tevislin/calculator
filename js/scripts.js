8e3814e7acece7ccc3270b1bc262bfa4c06d92ef
tevislin <tevislin18@gmail.com>
2021-02-19 04:38:26 +0300

    Added substraction and multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,10 @@ var add= function(a, b){
  };
  var a= parseInt(prompt("Enter first number"));
  var b= parseInt(prompt("Enter second number"));
- alert("The answer is "+add(a,b))
+ var sub= function(a,b){
+     return a-b;
+ };
+ var mult= function(a, b){
+     return a*b;
+ };
+ alert("The answer is "+mult(a,b))
