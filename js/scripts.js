9146bd663324a0473e51a03d0ec81cae7a492c13
tevislin <tevislin18@gmail.com>
2021-02-19 04:31:13 +0300

    Added html css and java files

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add= function(a, b){
+    return a + b;
+ };
+ var a= parseInt(prompt("Enter first number"));
+ var b= parseInt(prompt("Enter second number"));
+ alert("The answer is "+add(a,b))
